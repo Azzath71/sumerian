5fa152cfcd12b8325ba1bc9724009cf95a5a273f
adalith <adalithprojects@outlook.com>
2026-09-19 23:04:09 -0500

    add github page

--- src/sumerian.adb
+++ src/sumerian.adb
@@ -1,4 +1,17 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
 procedure Sumerian is
 begin
-   null;
+   --  ===First..part===
+   --  Introduction
+   --  Gameplay
+   --  Check win conditions
+   --  ===Second.part===
+   --  Introduction
+   --  Gameplay
+   --  Check win conditions
+   --  ===Third..part===
+   --  Introduction
+   --  Gameplay
+   --  Check win conditions
 end Sumerian;
